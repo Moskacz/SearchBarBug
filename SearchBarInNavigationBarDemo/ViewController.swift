11199dc39ef75f7384ff09cd8139a9e5fe6cb4df
Michał Moskała <moskala.michal@gmail.com>
2017-09-20 20:03:35 +0200

    Initial Commit

---
+++ SearchBarInNavigationBarDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SearchBarInNavigationBarDemo
+//
+//  Created by Michal Moskala on 20.09.2017.
+//  Copyright © 2017 Michal Moskala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
